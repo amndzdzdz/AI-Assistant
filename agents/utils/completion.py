393f6a0c2689910f653d9d06d5e55341a7820d53
amndzdzdz <zorrofx@gmx.net>
2025-04-14 10:28:55 +0200

    added extraction utils / added ChatHistory class

--- agents/utils/completion.py
+++ agents/utils/completion.py
@@ -23,9 +23,33 @@ def update_chat_history(history: list, msg: str, role: str):
         role (str): The role type (e.g. 'user', 'assistant', 'system')
     """
     history.append(build_prompt_structure(prompt=msg, role=role))
+
+class ChatHistory(list):
+    def __init__(self, messages: list | None = None, total_length: int = -1):
+        """Initialise the queue with a fixed total length.
+
+        Args:
+            messages (list | None): A list of initial messages
+            total_length (int): The maximum number of messages the chat history can hold.
+        """
+        if messages is None:
+            messages = []
+
+        super().__init__(messages)
+        self.total_length = total_length
+
+    def append(self, msg: str):
+        """Add a message to the queue.
+
+        Args:
+            msg (str): The message to be added to the queue
+        """
+        if len(self) == self.total_length:
+            self.pop(0)
+        super().append(msg)
     
 
-class InitializeChatHistory(list):
+class InitializeChatHistory(ChatHistory):
     def __init__(self, messages: list | None = None, total_length: int = -1):
         """Initialise the queue with a fixed total length.
 
