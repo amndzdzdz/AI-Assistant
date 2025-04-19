b7af7adcf4b562b920697136ebf318e4659fcea3
amndzdzdz <zorrofx@gmx.net>
2025-04-19 09:14:26 +0200

    added post request upon message enter

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from utils.frontend_utils import post_prompt_to_backend
+from utils.config_loader import ConfigLoader
+
+CONFIG_PATH = "configs/config.yaml"
+
+config = ConfigLoader(config_path=CONFIG_PATH)
 
 st.title("Your personal assisstant Nima!")
 
@@ -11,12 +17,15 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt, "image": ""})
+
+
+    response = post_prompt_to_backend(st.session_state.messages, config.get('BACKEND_API_URL'), image=None)
 
     with st.chat_message("user"):
         st.markdown(prompt)
 
     #with st.chat_message("assistant"):
 
-    #    response = st.write_stream(api_response)
+    #   response = st.write_stream(api_response)
     #st.session_state.messages.append({"role": "assistant", "content": response})
