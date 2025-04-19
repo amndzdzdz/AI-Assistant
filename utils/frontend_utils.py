cfcdd695b9f6ad4c7b5bf7f78709900afc93d808
amndzdzdz <zorrofx@gmx.net>
2025-04-19 07:48:54 +0200

    added function to send a post request to the backend

---
+++ utils/frontend_utils.py
@@ -0,0 +1,11 @@
+import requests
+from typing import List, Dict, Optional
+
+def post_prompt_to_backend(chat_history: List[Dict], api_endpoint: str, image: Optional[str]) -> Dict:
+    payload = {
+        "chat_history": chat_history
+    }
+
+    response = requests.post(api_endpoint, json = payload)
+
+    return response
