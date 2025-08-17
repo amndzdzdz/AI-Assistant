5a66402af7fa9c44c1c19b3104921c4fcecc22ca
amndzdzdz <zorrofx@gmx.net>
2025-08-17 21:43:29 +0200

    some refactoring

---
+++ src/utils/config_loader.py
@@ -0,0 +1,23 @@
+import yaml
+
+class ConfigLoader(object):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, config_path: str) -> None:
+        with open(config_path) as file:
+            self._config = yaml.safe_load(file)
+
+    def get(self, key: str):
+        config = self._config
+        for config_key in config:
+            try:
+                return config[config_key][key]
+            except:
+                raise KeyError(f"The config does not contain the key '{key}'")
+            
+config = ConfigLoader(config_path="configs/config.yaml")
